aec25832f20a5ff5b84b69ce25f85f16661ee092
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-09-09 14:28:47 -0400

    Added stub functions and stub test functions
    Added stubs for splitTip(), testSplitTip(), and testBMI(), testRetire()

--- src/main.java
+++ src/main.java
@@ -36,7 +36,7 @@ public class main {
 				distance();
 				break;
 			case 4:
-				//System.out.println("Case 4");
+				splitTip();
 				break;
 			case 5:
 				System.out.println("Exiting. Thank You.");
@@ -49,6 +49,11 @@ public class main {
 			}
 		}
 
+	}
+	
+	public static float splitTip ()
+	{
+		return 0;
 	}
 
 	public static float distance ()
